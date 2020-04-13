5eaf51db02d95ea1bcbf97a88ab17a92d45fdc2f
pivotmobileapp <jupiter@pivotmobileapp.com>
2020-04-13 11:31:40 -0400

    Initial commit

---
+++ platforms/browser/www/js/config.js
@@ -0,0 +1,6 @@
+var krms_config ={			
+	'ApiUrl':"https://paymeal.app/singlemerchant/api",			
+	'DialogDefaultTitle':"Packed",			
+	'MerchantKeys':"ab8aeac94c90cf35786d3a86580f4fe8",
+	'debug': false
+};
